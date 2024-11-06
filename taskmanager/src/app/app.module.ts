fec158541c9d8f1b2d4f3c97356b05445e4bc975
vamsi7997 <103129339+vamsi7997@users.noreply.github.com>
2024-11-06 22:26:59 +0530

    toogle bar added

--- taskmanager/src/app/app.module.ts
+++ taskmanager/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
